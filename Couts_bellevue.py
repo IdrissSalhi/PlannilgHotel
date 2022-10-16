8b56d77ba72f7eb997175cb68d4a5d6a5cdca879
IdrissSalhi <idriss_salhi@hotmail.fr>
2022-10-16 14:37:44 +0200

    Added title column for couts_bellevue.py

--- Couts_bellevue.py
+++ Couts_bellevue.py
@@ -1,6 +1,8 @@
+from textwrap import fill
 from Src.Model import *
 from Src.Controller import *
 import locale
+from Src.Config import *
 
 
 locale.setlocale(locale.LC_TIME, 'fr_FR')
@@ -14,13 +16,53 @@ class Cout_View :
         self.window.title("Couts reservation")
         self.window.resizable(False, False)
         self.window.state("zoomed")
-        self.master_couts = Frame(master = self.window)
+        self.master_couts = Frame(master = self.window, highlightbackground = "blue", highlightthickness = 2)
+        self.master_titre= Frame(master = self.window, highlightbackground = "green", highlightthickness = 2)
+        self.pixel = PhotoImage(width = 1, height = 1)
+
+        
         self._reservation = reservation
-    
+
+        #to do   deux frames : - titre (colonne de gauche)
+        #                       - zone slider (les couts, le reste des colonnes) 
+        #                       - frame boutons (quitter, sauvegarder, GENERER FACTURE)
+        #           
     def initialisation (self) :
-        self.master_couts.pack(fill=BOTH, side=LEFT,expand = True)
         
+        #remplir les titres et peupler zone des slider
+        self.master_titre.configure(width = 1000)
+        Label(master = self.master_titre, text = "DATE", font = font.Font(family = POLICE_BOUTONS, size =POLICE_BOUTONS_TAILLE), image = self.pixel, height= self.element_height, width = self.element_width, compound = CENTER).grid(column = 0, row = 0, sticky= NSEW)
+        b_names = ["Chambre", "Petit-dejeuner", "Téléphone", "Bar", "", "Taxe Séjour"]
+        for i in range(len(b_names)) :
+            Button(master = self.master_titre, text = b_names[i], font = font.Font(family = POLICE_BOUTONS, size =POLICE_BOUTONS_TAILLE), image = self.pixel, height= self.element_height, width = self.element_width, compound = CENTER).grid(column = 0, row = i+1, sticky= NSEW)
+        #TO DO : Ajouter commande bouton
         
+
+        data_stringvar = []
+        #début couts jour
+        jour_stringvar = []
+        Label(master= self.master_couts, text="").grid(column = 0, row = 0, sticky = W)
+        for i in range(len(b_names)) :
+            var = StringVar()
+            Spinbox(master= self.master_couts, textvariable = var, from_ = 0 , to = 10000).grid(column = 0, row = i+1, sticky = W)
+            jour_stringvar.append(var)
+
+
+        
+        data_stringvar.append(jour_stringvar)
+        #fin couts jour
+
+
+
+
+
+
+
+
+        self.master_titre.grid(column = 0, row = 0)
+        self.master_couts.grid(column = 1, row = 0, columnspan = 7, sticky=NSEW)
+
+
         self.window.mainloop()
         
 
